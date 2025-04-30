ff965465f099e5d3b5f99c8549e99e8a37f91fff
fiko942 <tobellord@gmail.com>
2025-04-30 11:48:19 +0700

    Add setupDikemas method to fetch and update "being packed" orders from Tokopedia

--- src/class/AccountInformation.ts
+++ src/class/AccountInformation.ts
@@ -174,6 +174,7 @@ export default class AccountInformation {
       account = await this.setupShippingOrder(account, headers);
       account = await this.setupBalance(account, headers);
       account = await this.setupComplaint(account, headers);
+      account = await this.setupDikemas(account, headers);
       account.lastUpdated = moment().tz(this.tz).unix();
       if (autoUpdate && database) {
         this.updateData(account, database);
@@ -321,6 +322,67 @@ export default class AccountInformation {
     return account
   }
 
+
+  /**
+   * Fetches the list of "being packed" (dikemas) orders from Tokopedia
+   * for the given account, calculates the total order value (potency),
+   * and determines the nearest shipping deadline.
+   *
+   * This function sends a POST request with a specific payload to retrieve
+   * orders in the "dikemas" state. It updates the account object with:
+   * - the number of orders,
+   * - the total value of those orders,
+   * - and the earliest latest shipping time (epoch).
+   * A notification is shown if new orders are detected.
+   *
+   * @param account - The account containing Tokopedia auth and order tracking info.
+   * @param header - The HTTP headers used for the API call (includes tokens).
+   * @returns A Promise resolving to the updated account object.
+   */
+  private async setupDikemas(account: StructAccount, header: any): Promise<StructAccount> {
+    const url = `https://seller-id.tokopedia.com/api/fulfillment/order/list?locale=id-ID&language=id&oec_seller_id=${account.auth.oecSellerId}&aid=4068&app_name=i18n_ecom_shop&fp=${account.auth.fp}&device_platform=web&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=en-US&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F135.0.0.0%20Safari%2F537.36&browser_online=true&timezone_name=Asia%2FJakarta&msToken=${account.auth.msToken}&X-Bogus=${account.auth.XBogus}&_signature==${account.auth.signature}`
+    const payload = {"sort_info":"6","search_condition":{"condition_list":{"search_tab":{"value":["110"]}}},"count":50,"pagination_type":0,"offset":0,"search_cursor":"","extra_data_list":["48_hours_dispatch_tag","split_combine_tag_v1","free_sample_tag_v1","hazmat_order_tag","made_to_order_tag","pre_order_tag","pre_sell_tag","zero_lottery_tag","gift_insurance_tag","internal_purchase_tag","replacement_order_tag_v1","risk_order_tag_v1","combo_sku_tag","refundable_sample_tag","split_package_type_tag","two_day_delivery","DT_order"]}
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        ...header,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json()
+    console.log({data})
+    if(data.data.total_count === 0) {
+      account.dikemasCount = 0
+      account.dikemasPotency = 0
+      account.dikemasEpoch = 0
+      return account
+    }
+    const orders: any = data.data.main_orders
+    let orderPotency: number = 0
+    let orderDeadline: number[] = []
+    for(const order of orders) {
+      orderPotency += parseInt(order.price_module.grand_total.price_val)
+      orderDeadline.push(parseInt(order.trade_order_module.latest_tts_time))
+    }
+    const orderCount = orders.length
+    if(orderCount > account.dikemasCount) {
+      const diff = orderCount - account.dikemasCount
+      const title =
+        `${account.name}` +
+        (this.account.getFirstGroupName(account.id)
+          ? ` - ${this.account.getFirstGroupName(account.id)}`
+          : "");
+      this.notification.show({
+        title,
+        message: `${diff} Pesanan sedang dikemas`,
+      });
+    }
+    account.dikemasCount = orderCount
+    account.dikemasPotency = orderPotency
+    account.dikemasEpoch = orderCount === 0 ? 0 : Math.min(...orderDeadline)
+    return account
+  }
 
 
   /**
