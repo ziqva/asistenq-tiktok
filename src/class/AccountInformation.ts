884e7ac41c5960f83c217ac009c2697f262d4bd0
fiko942 <tobellord@gmail.com>
2025-04-30 09:50:01 +0700

    Implement setupShippingOrder method to fetch and process shipping orders from the API

--- src/class/AccountInformation.ts
+++ src/class/AccountInformation.ts
@@ -186,6 +186,36 @@ export default class AccountInformation {
   }
 
   private async setupShippingOrder(account: StructAccount, headers: any): Promise<StructAccount> {
+    const url = `https://seller-id.tokopedia.com/api/fulfillment/order/list?locale=id-ID&language=id&oec_seller_id=${account.auth.oecSellerId}&aid=4068&app_name=i18n_ecom_shop&fp=${account.auth.fp}&device_platform=web&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=en-US&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F135.0.0.0%20Safari%2F537.36&browser_online=true&timezone_name=Asia%2FJakarta&msToken=${account.auth.msToken}&X-Bogus=${account.auth.XBogus}&_signature=${account.auth.signature}`
+    const payload = {"sort_info":"6","search_condition":{"condition_list":{"search_tab":{"value":["102"]}}},"count":50,"pagination_type":0,"offset":0,"extra_data_list":["48_hours_dispatch_tag","split_combine_tag_v1","free_sample_tag_v1","hazmat_order_tag","made_to_order_tag","pre_order_tag","pre_sell_tag","zero_lottery_tag","gift_insurance_tag","internal_purchase_tag","replacement_order_tag_v1","risk_order_tag_v1","combo_sku_tag","refundable_sample_tag","split_package_type_tag","two_day_delivery","DT_order"]}
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        ...headers,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data: any = await response.json()
+    const orders: any = data.data.main_orders
+    let orderPotency: number = 0
+    for(const order of orders) {
+      orderPotency += parseInt(order.price_module.grand_total.price_val)  
+    }
+    if(orders.length > account.dikirimCount) {
+      const diff = orders.length - account.dikirimCount
+      const title =
+        `${account.name}` +
+        (this.account.getFirstGroupName(account.id)
+          ? ` - ${this.account.getFirstGroupName(account.id)}`
+          : "");
+      this.notification.show({
+        title,
+        message: `${diff} Pesanan telah dikirim`,
+      });
+    }
+    account.dikirimCount = orders.length
+    account.dikirimPotency = orderPotency
     return account
   }
 
@@ -234,10 +264,10 @@ export default class AccountInformation {
         title,
         message: `${diff} Pesanan baru`,
       });
-      account.orderCount = orderCount
-      account.orderPotency = orderPotency
-      account.orderEpoch = orderCount === 0 ? 0 : Math.min(...orderDeadline)
     }
+    account.orderCount = orderCount
+    account.orderPotency = orderPotency
+    account.orderEpoch = orderCount === 0 ? 0 : Math.min(...orderDeadline)
     return account
   }
 
