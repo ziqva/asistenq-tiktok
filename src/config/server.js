42e8894e11cbbccc96c1fd901e25996eb6576c5f
fiko942 <tobellord@gmail.com>
2025-04-30 23:35:32 +0700

    Update server configuration to change API and socket base URLs to localhost:9184

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,11 @@
 export default {
     api: {
-        base: "http://localhost:9183",
+        base: "http://localhost:9184",
         chat: "https://srv-ziqlabs-1.my.id/live-chat"
         // chat: "http://localhost:2045/live-chat"
     },
     socket: {
-        base: "ws://localhost:9183",
+        base: "ws://localhost:9184",
         chat: "ws://45.76.183.58:2045"
         // chat: "ws://localhost:2045"
     }
