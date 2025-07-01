1c7bc90e7043e0f5e1bc5d28808b33e2d5cb79a4
fiko942 <tobellord@gmail.com>
2025-07-01 21:32:22 +0700

    Refactor setupDikemas method to improve order fetching and processing logic

--- src/class/AccountInformation.ts
+++ src/class/AccountInformation.ts
@@ -425,19 +425,50 @@ export default class AccountInformation {
     })
     const data = await response.json()
     if(!data.data) { console.error('setupDikemas error: ', data); return account; }
-    if(data.data.total_count === 0) {
+    
+    let orders: any = data.data.main_orders || []
+    let orderPotency: number = 0
+    let orderDeadline: number[] = []
+    
+
+    // filter packaged orders
+    const url2 = `https://seller-id.tokopedia.com/api/fulfillment/order/list?locale=id-ID&language=id&oec_seller_id=${account.auth.oecSellerId}&aid=4068&app_name=i18n_ecom_shop&fp=${account.auth.fp}&device_platform=web&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=en-US&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F135.0.0.0%20Safari%2F537.36&browser_online=true&timezone_name=Asia%2FJakarta&msToken=${account.auth.msToken}&X-Bogus=${account.auth.XBogus}&_signature==${account.auth.signature}`
+    const payload2 = {"sort_info":"1","search_condition":{"condition_list":{"order_status":{"value":["2"]},"search_tab":{"value":["101"]}}},"count":20,"pagination_type":0,"offset":0,"search_cursor":"","extra_data_list":["48_hours_dispatch_tag","split_combine_tag_v1","free_sample_tag_v1","hazmat_order_tag","made_to_order_tag","pre_order_tag","pre_sell_tag","zero_lottery_tag","gift_insurance_tag","internal_purchase_tag","replacement_order_tag_v1","risk_order_tag_v1","combo_sku_tag","refundable_sample_tag","split_package_type_tag","two_day_delivery","DT_order"]}
+    const response2 = await fetch(url2, {
+      method: "POST",
+      headers: {
+        ...header,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload2)
+    })
+    if(response.ok) {
+      const data = await response2.json()
+      console.log({data})
+      if(data.data) {
+        const _orders = data.data.main_orders
+        orders.push(..._orders)
+      }
+    } else {
+      console.error(response.status, response.statusText);
+    }
+
+    for(const order of orders) {
+      try {
+        orderPotency += parseInt(order.price_module.grand_total.price_val)
+        orderDeadline.push(parseInt(order.trade_order_module.latest_tts_time))
+      } catch(err) {
+        console.error(err)
+      }
+    }
+
+    if(orders === 0) {
       account.dikemasCount = 0
       account.dikemasPotency = 0
       account.dikemasEpoch = 0
       return account
     }
-    const orders: any = data.data.main_orders
-    let orderPotency: number = 0
-    let orderDeadline: number[] = []
-    for(const order of orders) {
-      orderPotency += parseInt(order.price_module.grand_total.price_val)
-      orderDeadline.push(parseInt(order.trade_order_module.latest_tts_time))
-    }
+
     const orderCount = orders.length
     if(orderCount > account.dikemasCount) {
       const diff = orderCount - account.dikemasCount
